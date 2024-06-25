42243e5361ef70dad8ed2e0e4a96fd3a308478a3
Simon Porritt <simon@jsplumbtoolkit.com>
2024-06-26 07:53:20 +1000

    add choice option component

---
+++ angular/src/app/choice-option.component.ts
@@ -0,0 +1,28 @@
+import {BasePortComponent} from "@jsplumbtoolkit/browser-ui-angular"
+import {Component, ElementRef} from "@angular/core"
+
+@Component({
+  selector:`choice-option`,
+  template:`<div class="jtk-chatbot-choice-option"
+                 data-jtk-source="true"
+                 data-jtk-port-type="choice"
+                 [attr.data-jtk-port]="obj['id']"
+                 (click)="inspectChoice(obj['id'])">
+    {{obj['label']}}
+    <div class="jtk-choice-delete" (click)="removeChoice(obj['id'])"></div>
+  </div>`
+})
+export class ChoiceOptionComponent extends BasePortComponent {
+
+  constructor(el: ElementRef) {
+    super(el);
+  }
+
+  inspectChoice(id:string) {
+    this.parent.toolkit.setSelection(this.parent.getNode().getPort(id))
+  }
+
+  removeChoice(id:string) {
+    this.parent.toolkit.removePort(this.parent.getNode(), id)
+  }
+}
