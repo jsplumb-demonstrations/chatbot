d6b133c5560f10d881e3106747043295a16dc62c
Simon Porritt <simon@jsplumbtoolkit.com>
2024-06-26 07:52:59 +1000

    switch to using choice option component for choices

--- angular/src/app/choice.component.ts
+++ angular/src/app/choice.component.ts
@@ -7,31 +7,16 @@ import { uuid } from "@jsplumbtoolkit/browser-ui"
     <div class="jtk-delete" (click)="removeNode()"></div>
     {{obj['message']}}
     <div class="jtk-choice-add" (click)="addChoice()"></div>
-    <div class="jtk-chatbot-choice-option" *ngFor="let choice of obj['choices']"
-         data-jtk-source="true"
-         data-jtk-port-type="choice"
-         [attr.data-jtk-port]="choice['id']"
-         (click)="inspectChoice(choice.id)">
-      {{choice['label']}}
-      <div class="jtk-choice-delete" (click)="removeChoice(choice.id)"></div>
-    </div>
+    <choice-option *ngFor="let choice of obj['choices']" [obj]="choice" [parent]="this"></choice-option>
   </div>`
 })
 export class ChoiceComponent extends BaseNodeComponent {
-
-  inspectChoice(id:string) {
-    this.toolkit.setSelection(this.getNode().getPort(id))
-  }
 
   addChoice() {
     this.toolkit.setSelection(this.toolkit.addPort(this.getNode(), {
       id:uuid(),
       label:"Choice"
     }))
-  }
-
-  removeChoice(id:string) {
-    this.toolkit.removePort(this.getNode(), id)
   }
 
 }
